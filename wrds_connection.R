bf41b8bf9b4ec89244f76740d726ecb75eb0e04e
otidin <onurtidin@gmail.com>
2020-02-13 16:58:25 +0100

    connection to WRDS

--- wrds_connection.R
+++ wrds_connection.R
@@ -16,3 +16,23 @@ sql <- "select * from CRSP.MSF"#the query to be passed to WRDS
 # sql <- "select * from CRSP.MONTHLY_TNA_RET_NAV" #the query to be passed to WRDS
 # sql <- "select * from CRSP.FUND_HDR" #the query to be passed to WRDS
 # sql <- "select * from CRSP.MSF"
+
+
+names <-                 c("FUND_HDR", # 15 sec
+                           "FUND_STYLE", # 17 sec
+                           "FUND_FEES", # 50 sec
+                           "FUND_NAMES", #
+                           "MONTHLY_RETURNS", #
+                           "MONTHLY_NAV", #
+                           "MONTHLY_TNA", #
+                           "MONTHLY_TNA_RET_NAV") #
+
+i=1
+sql<-paste("select * from CRSP.",names[i], sep="")
+tic()
+res <- dbSendQuery(wrds, sql)
+data<- dbFetch(res, n=40)
+# data<- dbFetch(res)
+toc()
+dbClearResult(res)
+View(data)
